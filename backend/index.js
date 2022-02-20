bae92113cfbbb6d427f888bd467680814aa7e477
mehmeteminsalim <mehmeteminsalim@gmail.com>
2022-02-20 16:40:12 +0300

    server-changed

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,8 @@ app.get("/priorities", (req, res) => {
   res.send(priorities);
 });
 
-app.listen(3002, () => {
-  console.log("Server listening on port 3002");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server listening");
 });
